dd9102b54a6f8a064840eb6f3356473422a1fd97
shuntaochen <974225666@qq.com>
2018-08-29 17:50:55 +0800

    fix exception console writing

--- rename/Program.cs
+++ rename/Program.cs
@@ -25,39 +25,43 @@ namespace rename
 
             void Rename(string input, string oldString, string newString)
             {
-                var files = Directory.GetFiles(input);
-                foreach (var file in files)
+                try
                 {
-                    FileInfo fileInfo = new FileInfo(file);
-                    var content = File.ReadAllText(file);
-                    if (content.Contains(oldString))
-                        File.WriteAllText(file, content.Replace(oldString, newString));
+                    var files = Directory.GetFiles(input);
+                    foreach (var file in files)
+                    {
+                        FileInfo fileInfo = new FileInfo(file);
+                        var content = File.ReadAllText(file);
+                        if (content.Contains(oldString))
+                            File.WriteAllText(file, content.Replace(oldString, newString));
 
-                    if (fileInfo.Name.Contains(oldString))
-                    {
-                        var newName = fileInfo.Name.Replace(oldString, newString);
-                        if (!string.IsNullOrEmpty(newName))
-                            fileInfo.MoveTo(fileInfo.Directory.FullName + "\\" + newName);
-                        else Console.WriteLine("Empty name in file path after replacement!");
-                    }
-                }
-                var dirs = Directory.GetDirectories(input);
-                if (dirs.Length > 0)
-                {
-                    foreach (var dir in dirs)
-                    {
-                        Rename(dir, oldString, newString);
-                        DirectoryInfo dirInfo = new DirectoryInfo(dir);
-                        if (dirInfo.Name.Contains(oldString))
+                        if (fileInfo.Name.Contains(oldString))
                         {
-                            var name = dirInfo.Name.Replace(oldString, newString);
-                            if (!string.IsNullOrEmpty(name))
-                                dirInfo.MoveTo(dirInfo.Parent.FullName + "\\" + name);
-                            else Console.WriteLine("Empty name in directory path after replacement!");
+                            var newName = fileInfo.Name.Replace(oldString, newString);
+                            if (!string.IsNullOrEmpty(newName))
+                                fileInfo.MoveTo(fileInfo.Directory.FullName + "\\" + newName);
+                            else Console.WriteLine("Empty name in file path after replacement!");
                         }
+                    }
+                    var dirs = Directory.GetDirectories(input);
+                    if (dirs.Length > 0)
+                    {
+                        foreach (var dir in dirs)
+                        {
+                            Rename(dir, oldString, newString);
+                            DirectoryInfo dirInfo = new DirectoryInfo(dir);
+                            if (dirInfo.Name.Contains(oldString))
+                            {
+                                var name = dirInfo.Name.Replace(oldString, newString);
+                                if (!string.IsNullOrEmpty(name))
+                                    dirInfo.MoveTo(dirInfo.Parent.FullName + "\\" + name);
+                                else Console.WriteLine("Empty name in directory path after replacement!");
+                            }
 
+                        }
                     }
                 }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
             }
         }
     }
